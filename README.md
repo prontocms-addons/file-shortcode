495b8c50659184dc8106416112073ac228eb6842
Thomas Rasshofer <me@thomasrasshofer.com>
2013-09-15 01:03:07 +0200

    Moving to GitHub (http://prontocms.com/blog/github)

---
+++ README.md
@@ -0,0 +1,3 @@
+# File shortcode add-on for PRONTO.
+ 
+[http://prontocms.com/downloads/file-shortcode](http://prontocms.com/downloads/file-shortcode)
---
+++ shortcode.file.php
@@ -0,0 +1,24 @@
+<?php
+
+use Pronto\ShortcodeContainer;
+use Pronto\GlobalContainer;
+use Pronto\HelperContainer;
+
+ShortcodeContainer::add('file', function($attributes) {
+	$page = GlobalContainer::get('page');
+	$file = array_shift($attributes);
+	if(HelperContainer::relative($file)) {
+		$file = $page->folder().$file;
+	}	
+	$defaults = array(
+		'text' => basename($file)
+	);
+	$options = array_merge($defaults, $attributes);
+	$append = array();
+	foreach($options as $key => $val) {
+		$append[] = ' '.$key.'="'.html($val).'"';
+	}
+	return '<a href="'.$file.'"'.implode($append).'>'.html($options['text']).'</a>';
+});
+
+?>
